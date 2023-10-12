cd74a0f3441bcddf434d0a76c2b79a38c080b191
hoaithanhvo <115620157+hoaithanhvo@users.noreply.github.com>
2023-10-13 00:12:53 +0700

    push code to the github local

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 8888
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+
+app.use('/', (req, res) => { res.send('SERVER ONNN') })
+
+
+app.listen(port, () => {
+    console.log("server chạy " + port);
+})
